7319d447fa405cbc9a20f7944c94306cef61a96a
Andrej Romanov <a.romanov@godeltech.com>
2025-01-27 15:42:24 +0200

    Renamed table

--- src/main/java/com/example/exchange/controller/CurrencyController.java
+++ src/main/java/com/example/exchange/controller/CurrencyController.java
@@ -3,15 +3,13 @@ package com.example.exchange.controller;
 import com.example.exchange.dto.CurrencyDto;
 import com.example.exchange.service.CurrencyService;
 import io.swagger.v3.oas.annotations.Operation;
+import java.util.List;
 import lombok.AllArgsConstructor;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 
 @RestController
@@ -19,13 +17,14 @@ import java.util.List;
 @AllArgsConstructor
 public class CurrencyController {
 
-    private final CurrencyService currencyService;
+  private final CurrencyService currencyService;
 
-    @GetMapping
-    @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "Get all currencies", description = "This API endpoint returns all currencies.")
-    public List<CurrencyDto> getAllCurrency() {
-        return currencyService.getAllCurrencies();
-    }
+  @GetMapping
+  @ResponseStatus(HttpStatus.OK)
+  @Operation(summary = "Get all currencies",
+      description = "This API endpoint returns all currencies.")
+  public List<CurrencyDto> getAllCurrency() {
+    return currencyService.getAllCurrencies();
+  }
 
 }
--- src/main/resources/db/changelog/changeset/01-create-currencies.sql
+++ src/main/resources/db/changelog/changeset/01-create-currencies.sql
@@ -1,4 +1,4 @@
 CREATE TABLE currency (
                                       id BIGSERIAL PRIMARY KEY,
-                                      currency VARCHAR(3) NOT NULL UNIQUE
+                                      name VARCHAR(3) NOT NULL UNIQUE
 );
