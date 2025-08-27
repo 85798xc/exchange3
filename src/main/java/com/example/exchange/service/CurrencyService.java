013e91d41a89b14393b5b1c073734bb11c599ad0
Andrej Romanov <romanov454545@gmail.com>
2025-08-27 07:44:35 +0300

    Minor changes, new apy key

--- src/main/java/com/example/exchange/service/CurrencyService.java
+++ src/main/java/com/example/exchange/service/CurrencyService.java
@@ -5,30 +5,31 @@ import com.example.exchange.entity.Currency;
 import com.example.exchange.exception.CurrencyAlreadyExistException;
 import com.example.exchange.mapper.CurrencyMapper;
 import com.example.exchange.repository.CurrencyRepository;
-import java.util.List;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CurrencyService {
 
-  private final CurrencyRepository currencyRepository;
-  private final CurrencyMapper currencyMapper;
+    private final CurrencyRepository currencyRepository;
+    private final CurrencyMapper currencyMapper;
 
 
-  public List<CurrencyDto> getAllCurrencies() {
-    return currencyRepository.findAll().stream().map(currencyMapper::toDto).toList();
-  }
-
-  public CurrencyDto addCurrency(CurrencyDto currencyDto) {
-
-    if (currencyRepository.existsByName(currencyDto.currency())) {
-      throw new CurrencyAlreadyExistException(currencyDto.currency());
+    public List<CurrencyDto> getAllCurrencies() {
+        return currencyRepository.findAll().stream().map(currencyMapper::toDto).toList();
     }
 
-    Currency currency = currencyRepository.save(currencyMapper.toEntity(currencyDto));
+    public CurrencyDto addCurrency(CurrencyDto currencyDto) {
 
-    return currencyMapper.toDto(currency);
-  }
+        if (currencyRepository.existsByName(currencyDto.currency())) {
+            throw new CurrencyAlreadyExistException(currencyDto.currency());
+        }
+
+        Currency currency = currencyRepository.save(currencyMapper.toEntity(currencyDto));
+
+        return currencyMapper.toDto(currency);
+    }
 }
